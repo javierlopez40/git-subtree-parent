214baebe58ac0bde7977e025e9841d66e45346e6
Javier Lopez <javier.lopez@avoristravel.com>
2020-05-04 15:03:21 +0200

    #master
    Parent initial commit.

---
+++ src/main/java/com/example/demo/git/subtree/DemoGitSubtreeApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo.git.subtree;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoGitSubtreeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoGitSubtreeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demo/git/subtree/DemoGitSubtreeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo.git.subtree;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoGitSubtreeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
